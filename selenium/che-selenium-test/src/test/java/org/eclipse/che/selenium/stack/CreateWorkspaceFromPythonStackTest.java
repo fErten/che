71d520e4cf67f939f0a034a587b588bcea8a55dd
sskoryk <sskoryk@redhat.com>
2018-06-22 17:25:51 +0300

    rework CreateWorkspaceFromPythonStackTest test to use StackHelper

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/stack/CreateWorkspaceFromPythonStackTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/stack/CreateWorkspaceFromPythonStackTest.java
@@ -13,27 +13,13 @@ package org.eclipse.che.selenium.stack;
 import static java.lang.String.format;
 import static org.eclipse.che.commons.lang.NameGenerator.generate;
 import static org.eclipse.che.selenium.core.constant.TestProjectExplorerContextMenuConstants.ContextMenuCommandGoals.RUN;
-import static org.eclipse.che.selenium.core.constant.TestTimeoutsConstants.ELEMENT_TIMEOUT_SEC;
-import static org.eclipse.che.selenium.core.constant.TestTimeoutsConstants.WIDGET_TIMEOUT_SEC;
-import static org.eclipse.che.selenium.pageobject.ProjectExplorer.FolderTypes.PROJECT_FOLDER;
 import static org.eclipse.che.selenium.pageobject.dashboard.NewWorkspace.Stack.PYTHON;
 
 import com.google.inject.Inject;
 import org.eclipse.che.selenium.core.client.TestWorkspaceServiceClient;
-import org.eclipse.che.selenium.core.constant.TestProjectExplorerContextMenuConstants.ContextMenuCommandGoals;
 import org.eclipse.che.selenium.core.user.DefaultTestUser;
-import org.eclipse.che.selenium.core.webdriver.SeleniumWebDriverHelper;
-import org.eclipse.che.selenium.pageobject.CodenvyEditor;
-import org.eclipse.che.selenium.pageobject.Consoles;
-import org.eclipse.che.selenium.pageobject.Ide;
-import org.eclipse.che.selenium.pageobject.MavenPluginStatusBar;
-import org.eclipse.che.selenium.pageobject.NotificationsPopupPanel;
-import org.eclipse.che.selenium.pageobject.ProjectExplorer;
-import org.eclipse.che.selenium.pageobject.ToastLoader;
 import org.eclipse.che.selenium.pageobject.dashboard.Dashboard;
 import org.eclipse.che.selenium.pageobject.dashboard.NewWorkspace;
-import org.eclipse.che.selenium.pageobject.dashboard.ProjectSourcePage;
-import org.eclipse.che.selenium.pageobject.dashboard.workspaces.Workspaces;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -48,19 +34,10 @@ public class CreateWorkspaceFromPythonStackTest {
           "Finished language servers initialization, file path '/%s/%s'",
           PROJECT_NAME, PYTHON_FILE_NAME);
 
-  @Inject private Ide ide;
-  @Inject private Consoles consoles;
   @Inject private Dashboard dashboard;
-  @Inject private CodenvyEditor editor;
-  @Inject private Workspaces workspaces;
-  @Inject private ToastLoader toastLoader;
+  @Inject private StackHelper stackHelper;
   @Inject private NewWorkspace newWorkspace;
   @Inject private DefaultTestUser defaultTestUser;
-  @Inject private ProjectExplorer projectExplorer;
-  @Inject private ProjectSourcePage projectSourcePage;
-  @Inject private MavenPluginStatusBar mavenPluginStatusBar;
-  @Inject private NotificationsPopupPanel notificationsPopupPanel;
-  @Inject private SeleniumWebDriverHelper seleniumWebDriverHelper;
   @Inject private TestWorkspaceServiceClient workspaceServiceClient;
 
   @BeforeClass
@@ -75,71 +52,16 @@ public class CreateWorkspaceFromPythonStackTest {
 
   @Test
   public void checkWorkspaceCreationFromPythonStack() {
-    createWorkspaceWithProjectFromStack(PYTHON, WORKSPACE_NAME, PROJECT_NAME);
+    stackHelper.createWorkspaceWithProjectFromStack(PYTHON, WORKSPACE_NAME, PROJECT_NAME);
 
-    switchToIdeAndWaitWorkspaceIsReadyToUse();
+    stackHelper.switchToIdeAndWaitWorkspaceIsReadyToUse();
 
-    waitProjectInitialization(PROJECT_NAME);
+    stackHelper.waitProjectInitialization(PROJECT_NAME);
 
-    startCommandAndCheckMessageInTerminal(PROJECT_NAME, RUN, "run", "Hello, world!");
-    startCommandAndCheckMessageInTerminal(
+    stackHelper.startCommandAndCheckResult(PROJECT_NAME, RUN, "run", "Hello, world!");
+    stackHelper.startCommandAndCheckResult(
         PROJECT_NAME, RUN, "console-python3-simple:run", "Hello, world!");
 
-    checkLanguageServerInitialization(PROJECT_NAME, PYTHON_FILE_NAME, LS_INIT_MESSAGE);
-  }
-
-  private void createWorkspaceWithProjectFromStack(
-      NewWorkspace.Stack stack, String workspaceName, String projectName) {
-    dashboard.waitDashboardToolbarTitle();
-    dashboard.selectWorkspacesItemOnDashboard();
-    workspaces.clickOnAddWorkspaceBtn();
-    newWorkspace.waitToolbar();
-
-    newWorkspace.clickOnAllStacksTab();
-    newWorkspace.selectStack(stack);
-    newWorkspace.typeWorkspaceName(workspaceName);
-    projectSourcePage.clickOnAddOrImportProjectButton();
-    projectSourcePage.selectSample(projectName);
-    projectSourcePage.clickOnAddProjectButton();
-
-    newWorkspace.clickOnCreateButtonAndOpenInIDE();
-  }
-
-  private void switchToIdeAndWaitWorkspaceIsReadyToUse() {
-    seleniumWebDriverHelper.switchToIdeFrameAndWaitAvailability();
-    toastLoader.waitToastLoaderAndClickStartButton();
-    ide.waitOpenedWorkspaceIsReadyToUse();
-  }
-
-  private void waitProjectInitialization(String projectName) {
-    projectExplorer.waitItem(projectName);
-    notificationsPopupPanel.waitPopupPanelsAreClosed();
-    mavenPluginStatusBar.waitClosingInfoPanel();
-    projectExplorer.waitDefinedTypeOfFolder(projectName, PROJECT_FOLDER);
-    notificationsPopupPanel.waitPopupPanelsAreClosed();
-  }
-
-  private void startCommandAndCheckMessageInTerminal(
-      String projectName,
-      ContextMenuCommandGoals commandsGoal,
-      String commandName,
-      String expectedMessage) {
-    projectExplorer.waitAndSelectItem(projectName);
-    projectExplorer.invokeCommandWithContextMenu(commandsGoal, projectName, commandName);
-    consoles.waitTabNameProcessIsPresent(commandName);
-    consoles.waitProcessInProcessConsoleTree(commandName);
-    consoles.waitExpectedTextIntoConsole(expectedMessage, WIDGET_TIMEOUT_SEC);
-  }
-
-  private void checkLanguageServerInitialization(
-      String projectName, String fileName, String textInTerminal) {
-    projectExplorer.waitAndSelectItem(projectName);
-    projectExplorer.openItemByPath(projectName);
-    projectExplorer.openItemByPath(projectName + "/" + fileName);
-    editor.waitTabIsPresent(fileName);
-
-    // check a language server initialized
-    consoles.selectProcessByTabName("dev-machine");
-    consoles.waitExpectedTextIntoConsole(textInTerminal, ELEMENT_TIMEOUT_SEC);
+    stackHelper.checkLanguageServerInitialization(PROJECT_NAME, PYTHON_FILE_NAME, LS_INIT_MESSAGE);
   }
 }
