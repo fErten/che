1c75ec3cd68029e3dc611f75618cc99dd151eef3
sskoryk <sskoryk@redhat.com>
2018-06-26 14:37:56 +0300

    change CreateWorkspaceFromPHPStackTest test to work with 'web-php-gae-simple' project

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/stack/CreateWorkspaceFromPHPStackTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/stack/CreateWorkspaceFromPHPStackTest.java
@@ -17,12 +17,11 @@ import static org.eclipse.che.selenium.pageobject.dashboard.NewWorkspace.Stack.P
 import static org.openqa.selenium.Keys.ENTER;
 
 import com.google.inject.Inject;
+import java.util.ArrayList;
 import org.eclipse.che.selenium.core.client.TestWorkspaceServiceClient;
 import org.eclipse.che.selenium.core.user.DefaultTestUser;
 import org.eclipse.che.selenium.pageobject.CheTerminal;
-import org.eclipse.che.selenium.pageobject.CodenvyEditor;
 import org.eclipse.che.selenium.pageobject.Consoles;
-import org.eclipse.che.selenium.pageobject.ProjectExplorer;
 import org.eclipse.che.selenium.pageobject.dashboard.Dashboard;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -31,24 +30,29 @@ import org.testng.annotations.Test;
 /** @author Skoryk Serhii */
 public class CreateWorkspaceFromPHPStackTest {
   private static final String WORKSPACE_NAME = generate("workspace", 4);
-  private static final String PROJECT_NAME = "web-php-simple";
+  private static final String WEB_PHP_SIMPLE = "web-php-simple";
+  private static final String WEB_PHP_GAE_SIMPLE = "web-php-gae-simple";
   private static final String PHP_FILE_NAME = "index.php";
   private static final String LS_INIT_MESSAGE =
       format(
           "Finished language servers initialization, file path '/%s/%s'",
-          PROJECT_NAME, PHP_FILE_NAME);
+          WEB_PHP_SIMPLE, PHP_FILE_NAME);
+
+  private ArrayList<String> projects = new ArrayList<>();
+  String currentWindow;
 
   @Inject private Dashboard dashboard;
   @Inject private StackHelper stackHelper;
   @Inject private DefaultTestUser defaultTestUser;
-  @Inject private CodenvyEditor editor;
   @Inject private Consoles consoles;
   @Inject private CheTerminal terminal;
-  @Inject private ProjectExplorer projectExplorer;
   @Inject private TestWorkspaceServiceClient workspaceServiceClient;
 
   @BeforeClass
   public void setUp() {
+    projects.add(WEB_PHP_SIMPLE);
+    projects.add(WEB_PHP_GAE_SIMPLE);
+
     dashboard.open();
   }
 
@@ -59,32 +63,42 @@ public class CreateWorkspaceFromPHPStackTest {
 
   @Test
   public void checkWorkspaceCreationFromPHPStack() {
-    String currentWindow;
-
-    stackHelper.createWorkspaceWithProjectFromStack(PHP, WORKSPACE_NAME, PROJECT_NAME);
+    stackHelper.createWorkspaceWithProjectsFromStack(PHP, WORKSPACE_NAME, projects);
 
     currentWindow = stackHelper.switchToIdeAndWaitWorkspaceIsReadyToUse();
 
-    stackHelper.waitProjectInitialization(PROJECT_NAME);
+    stackHelper.waitProjectInitialization(WEB_PHP_SIMPLE);
+    stackHelper.waitProjectInitialization(WEB_PHP_GAE_SIMPLE);
+  }
 
+  @Test(priority = 1)
+  public void checkWebPhpSimpleCommands() {
     // open 'index.php' file and check Python language server initialization
-    stackHelper.checkLanguageServerInitialization(PROJECT_NAME, PHP_FILE_NAME, LS_INIT_MESSAGE);
+    stackHelper.checkLanguageServerInitialization(WEB_PHP_SIMPLE, PHP_FILE_NAME, LS_INIT_MESSAGE);
 
-    stackHelper.startCommandAndCheckResult(PROJECT_NAME, RUN, "run php script", "Hello World!");
+    stackHelper.startCommandAndCheckResult(WEB_PHP_SIMPLE, RUN, "run php script", "Hello World!");
 
     stackHelper.startCommandAndCheckResult(
-        PROJECT_NAME, RUN, "start apache", "Starting Apache httpd web server apache2");
+        WEB_PHP_SIMPLE, RUN, "start apache", "Starting Apache httpd web server apache2");
     stackHelper.startCommandAndCheckApp(currentWindow, "//*[text()='Hello World!']");
 
-    stackHelper.startCommandAndCheckResult(PROJECT_NAME, RUN, "restart apache", "...done");
+    stackHelper.startCommandAndCheckResult(WEB_PHP_SIMPLE, RUN, "restart apache", "...done");
     stackHelper.startCommandAndCheckApp(currentWindow, "//*[text()='Hello World!']");
 
     // start 'stop apache' command and check that apache not running
     stackHelper.startCommandAndCheckResult(
-        PROJECT_NAME, RUN, "stop apache", "Stopping Apache httpd web server apache2");
+        WEB_PHP_SIMPLE, RUN, "stop apache", "Stopping Apache httpd web server apache2");
     consoles.selectProcessByTabName("Terminal");
-    terminal.typeIntoTerminal("ps ax | grep 'service apache2 start'");
+    terminal.typeIntoTerminal("ps ax");
     terminal.typeIntoTerminal(ENTER.toString());
-    terminal.waitExpectedTextNotPresentTerminal("service apache2 start");
+    terminal.waitExpectedTextNotPresentTerminal("/usr/sbin/apache2 -k start");
+  }
+
+  @Test(priority = 1)
+  public void checkWebPhpGaeSimpleCommands() {
+    stackHelper.startCommandAndCheckResult(
+        WEB_PHP_GAE_SIMPLE, RUN, "web-php-gae-simple:run", "Starting admin server");
+    stackHelper.startCommandAndCheckApp(currentWindow, "//input[@value='Sign Guestbook']");
+    stackHelper.closeProcessTabWithAskDialog("web-php-gae-simple:run");
   }
 }
